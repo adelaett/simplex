fa715942268f8d1b677565ac5e6612cbf9f4721c
adelaett <90894311+adelaett@users.noreply.github.com>
2024-02-26 09:38:08 +0100

    simplification

--- solver/macro.ml
+++ solver/macro.ml
@@ -1,15 +1,17 @@
-(* range function. From https://stackoverflow.com/questions/243864/what-is-the-ocaml-idiom-equivalent-to-pythons-range-function *)
+(** range function. From https://stackoverflow.com/questions/243864/what-is-the-ocaml-idiom-equivalent-to-pythons-range-function *)
 let ( -- ) i j =
   let rec aux n acc = if n < i then acc else aux (n - 1) (n :: acc) in
   aux j []
 
 let id x = x
 
+(** Flatten an list of options. *)
 let get_somes l =
   List.fold_right (fun x l -> match x with Some i -> i :: l | None -> l) l []
 
+(** Give the last item of a list *)
 let last l = List.fold_left (fun _ x -> Some x) None l
-let apply_opt f o = match o with Some x -> Some (f x) | None -> None
+
 let reset x = x := 0
 
 let array_find_opt a x =
