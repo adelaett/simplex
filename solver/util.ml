40de092065839f583ec3ab6d0bdc2a005fb8328e
adelaett <90894311+adelaett@users.noreply.github.com>
2024-11-09 16:03:13 +0100

    renaming lots of things

--- solver/util.ml
+++ solver/util.ml
@@ -12,14 +12,18 @@ let get_somes l =
 (** Give the last item of a list *)
 let last l = List.fold_left (fun _ x -> Some x) None l
 
+(** [reset x] resets the reference to zero. *)
 let reset x = x := 0
 
+(** return the index of a value if the value was found *)
 let array_find_opt a x =
   let index = ref None in
   Array.iteri (fun i y -> if x = y then index := Some i) a;
   !index
 
+(** same as array_find_opt but raises an error if the value is not found. *)
 let array_find a x =
   match array_find_opt a x with Some i -> i | None -> raise Not_found
 
+(** [is_none o] returns true if [o] is [None] and false if it is [Some _]. *)
 let is_none x = match x with Some _ -> false | None -> true
